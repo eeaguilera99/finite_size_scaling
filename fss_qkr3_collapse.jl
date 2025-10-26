7e20c9fefefd4789c189579f047dcdd9cc4b1b92
Emilio <108963222+eeaguilera99@users.noreply.github.com>
2025-10-26 17:42:28 +0100

    adding variance funct for collapse quality

--- fss_qkr3_collapse.jl
+++ fss_qkr3_collapse.jl
@@ -20,7 +20,7 @@ Perform finite-time scaling collapse of Anderson transition data.
 dim = 2.3  # spatial dimension
 
 # Perform collapse
-res, shifts, X, Y, Yerr = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=6)
+res, shifts, X, Y, Yerr, s, s_rel = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=6)
 #=
 # Plot before collapse
 plt1 = plot(title="Raw data (before shifts)",
@@ -39,5 +39,5 @@ end
 display(plt2)
 
 #println("Optimal shifts ln ξ(K): ", shifts)
-println("Fit quality: ", res.minimum)
+println("Fit quality: ", s_rel)
 
