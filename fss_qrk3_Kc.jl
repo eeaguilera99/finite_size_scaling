894e354f715ed76f744ec5abba9a95c100242bd5
Your Name <eeaguilera99@gmail.com>
2025-10-28 14:04:55 +0100

    fix fit

--- fss_qrk3_Kc.jl
+++ fss_qrk3_Kc.jl
@@ -10,7 +10,7 @@ Returns best-fit parameters + standard errors from covariance matrix.
 """
 function fit_xi_offset_LsqFit(K_vals, xi; exclude_tol_frac=0.02, plotshow=true)
     # Model function
-    model(p, K) = p[1] .+ p[2] .* abs.(K .- p[4]).^(-p[3])   # ξ₀ + A|K−Kc|^{−ν}
+    model(K, p) = p[1] .+ p[2] .* abs.(K .- p[4]).^(-p[3])   # ξ₀ + A|K−Kc|^{−ν}
     names = ["ξ₀", "A", "ν", "Kc"]
 
     # Initial guess
@@ -34,7 +34,7 @@ function fit_xi_offset_LsqFit(K_vals, xi; exclude_tol_frac=0.02, plotshow=true)
     # Unpack results
     ξ0, A, ν, Kc = pbest
     err_ξ0, err_A, err_ν, err_Kc = perr
-
+    
     if plotshow
         # Plot raw data
         plt_raw = plot(K_vals, xi, seriestype=:scatter, ms=6,
@@ -58,16 +58,31 @@ end
 
 
 
-
-
 dim = 3  # spatial dimension
 
 # Perform collapse
 res, shifts, X, Y, Yerr = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=5)
 
 # ===== Example usage =====
- xi = exp.(shifts)
- results = fit_xi_offset_LsqFit(K_vals, xi; plotshow=true)
- println(results)
+xi = exp.(shifts)
+results = fit_xi_offset_LsqFit(K_vals, xi; plotshow=true)
+#=
+model(K, p) = p[1] .+ p[2] .* abs.(K .- p[4]).^(-p[3]) 
+ξ0₀ = minimum(xi)*0.5
+A₀  = maximum(xi)
+ν₀  = 1.5
+Kc₀ = K_vals[argmax(xi)]  # where ξ is largest
+p0 = [ξ0₀, A₀, ν₀, Kc₀]
+exclude_tol_frac=0.02
+
+# Mask out values too close to trial Kc₀
+ΔK = maximum(K_vals) - minimum(K_vals)
+mask = abs.(K_vals .- Kc₀) .> exclude_tol_frac*ΔK
+Kfit, ξfit = K_vals[mask], xi[mask]
+
+fit = curve_fit(model, Kfit, ξfit, p0)
+=#
+
+#println(results)
 
 
