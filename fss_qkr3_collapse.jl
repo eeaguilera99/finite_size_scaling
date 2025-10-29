f215573d0d7349c57e66c05d2d44337911f7214f
Your Name <eeaguilera99@gmail.com>
2025-10-29 15:12:12 +0100

    compare s vs dim

--- fss_qkr3_collapse.jl
+++ fss_qkr3_collapse.jl
@@ -17,7 +17,7 @@ Perform finite-time scaling collapse of Anderson transition data.
 - `(X, Y)`: Arrays of logarithmic coordinates.
 """
 
-dim = 3  # spatial dimension
+dim = 10  # spatial dimension
 
 # Perform collapse
 res, shifts, X, Y, Yerr, s_rel = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=5)
--- fss_qkr_scandim.jl
+++ fss_qkr_scandim.jl
@@ -2,31 +2,39 @@ include("fss_qkr3_timescaling.jl")  # for finite_time_scaling
 
 #code to loop over dimension values for best collapse
 
-d_values = 2:0.05:7
+d_values = 0:0.05:10
 
+collapse_quality_1 = Float64[]
 collapse_quality_2 = Float64[]
 #shift_dict = Dict{Float64, Vector{Float64}}()
 
 for dim in d_values
-    l_res, l_shifts, l_X, l_Y, l_Y_err, sX = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=1)
+    l_res, l_shifts, l_X, l_Y, l_Y_err, sX = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=5)
+    push!(collapse_quality_1, tot_sdeviation(l_shifts.*0, l_X, l_Y)[1]/tot_sdeviation(l_shifts, l_X, l_Y)[1])
     push!(collapse_quality_2, sX)
 end
 
-best_idx = argmin(collapse_quality)
-best_d = d_values[best_idx]
-best_val = collapse_quality[best_idx]
+
+function dim_scan(collapse_quality)
+    best_idx = argmin(collapse_quality)
+    best_d = d_values[best_idx]
+    best_val = collapse_quality[best_idx]
 
 
-println("\n✅ Best collapse found for:")
-println("   d_best = ", best_d)
-println("   min_val = ", best_val)
+    println("\n✅ Best collapse found for:")
+    println("   d_best = ", best_d)
+    println("   min_val = ", best_val)
 
-# Plot collapse quality vs dimension
-plt_quality = plot(d_values, collapse_quality, lw=1, marker=:o,
-    xlabel="Dimension d", ylabel="Collapse quality (min_val)",
-    title="Quality of scaling collapse vs dimension d")
-scatter!(plt_quality, [best_d], [best_val], label="Best d = $(round(best_d, digits=2))", markersize=8)
-display(plt_quality)
+    # Plot collapse quality vs dimension
+    plt_quality = plot(d_values, collapse_quality, lw=1, marker=:o,
+        xlabel="Dimension d", ylabel="Collapse quality (min_val)",
+        title="Quality of scaling collapse vs dimension d")
+    scatter!(plt_quality, [best_d], [best_val], label="Best d = $(round(best_d, digits=2))", markersize=8)
+    display(plt_quality)
+end
+
+dim_scan(collapse_quality_1)
+dim_scan(collapse_quality_2)
 
 #=
 # Optional: Plot best collapse curves
