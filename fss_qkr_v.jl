82e10c9c5bc2d03514a5c19fb393a7d066baa40e
Your Name <eeaguilera99@gmail.com>
2025-10-30 13:10:51 +0100

    only v try

--- fss_qkr_v.jl
+++ fss_qkr_v.jl
@@ -9,10 +9,10 @@ function fit_nu_vs_t(t_vals, Y, xi; ngrid=400, n_kicks_i=5, n_kicks_f=0)
     app_Kc_index = argmax(xi)
     Ln_Λ_vals = Y[app_Kc_index, :]
 
-    model(t,p) = p[1] .+ p[2].*t.^(1/(3*p[3]))  # model: ln(Λ) = ln(ξ0) + A * t^(1/(3*ν))
+    model(t,p) = t.^(1/(3*p[1]))  # model: ln(Λ) = ln(ξ0) + A * t^(1/(3*ν))
 
-    fit = curve_fit(model, t_vals, Ln_Λ_vals, [0.0, 1.0, 1.0])
-    ν_fit = fit.param[3]
+    fit = curve_fit(model, t_vals, Ln_Λ_vals, [1.0])
+    ν_fit = fit.param[1]
 
     plt_fit = plot(t_vals, Ln_Λ_vals, seriestype=:scatter, xscale=:ln, yscale=:ln, label="ν=$(ν_fit)", xlabel="t", ylabel="ln(Λ(Kc,t))",
         title="Fit of ln(Λ) vs t at Kc")
