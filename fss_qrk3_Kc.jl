29f8ee2e685a4cc5046506dc162e4ae3cfe58d7c
Your Name <eeaguilera99@gmail.com>
2025-10-31 13:47:04 +0100

    Working good

--- fss_qrk3_Kc.jl
+++ fss_qrk3_Kc.jl
@@ -58,7 +58,7 @@ function fit_xi_offset_LsqFit(K_vals, xi; exclude_tol_frac=0.02, plotshow=true)
 end
 
 
-dim = 3  # spatial dimension
+dim = 1  # spatial dimension
 
 # Perform collapse
 res, shifts, X, Y, Yerr = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=5)
