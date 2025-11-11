2a2dc5a30b9e74c5b48942d4201aa8ca05881e77
Your Name <eeaguilera99@gmail.com>
2025-11-11 16:26:20 +0100

    plotting raw data

--- fss_qkr3_collapse.jl
+++ fss_qkr3_collapse.jl
@@ -20,7 +20,7 @@ Perform finite-time scaling collapse of Anderson transition data.
 dim = 3  # spatial dimension
 
 # Perform collapse
-res, shifts, X, Y, Yerr, s_rel = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=0, n_kicks_f=0)
+res, shifts, X, Y, Yerr, s_rel = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=1, n_kicks_f=0)
 
 # Plot before collapse
 plt1 = plot(title="Raw data",
