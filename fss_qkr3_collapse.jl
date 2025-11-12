64d6507ec0cee13dc0aa2cc679d1b928caf71421
Your Name <eeaguilera99@gmail.com>
2025-11-12 16:28:24 +0100

    plots nc and k

--- fss_qkr3_collapse.jl
+++ fss_qkr3_collapse.jl
@@ -18,26 +18,48 @@ Perform finite-time scaling collapse of Anderson transition data.
 """
 
 dim = 3  # spatial dimension
+a_s = L"350a_0"  # scattering length label for plots
 
 # Perform collapse
-res, shifts, X, Y, Yerr, s_rel = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=1, n_kicks_f=0)
+res1, shifts1, X1, Y1, Yerr1, s_rel1 = finite_time_scaling(K_vals, t_vals, apply_mov_av_matrix(p2_mat), p2_err_mat; d=dim, n_kicks_i=1, n_kicks_f=0)
+res2, shifts2, X2, Y2, Yerr2, s_rel2 = finite_time_scaling(K_vals, t_vals, apply_mov_av_matrix(nc_mat), p2_err_mat; d=dim, n_kicks_i=1, n_kicks_f=0)
 
-# Plot before collapse
-plt1 = plot(title="Raw data",
+# plots for ⟨p²⟩
+plt1 = plot(title="Raw data "*L"E_k, a_s="*a_s,
     xlabel="ln(t^(-1/d))", ylabel="ln(Λ)")
 for (i,K) in enumerate(K_vals)
-    plot!(plt1, X[:], Y[i,:], marker=:o, label="K="*string(round(K, digits=3)))#, yerror=Yerr[i,:]
+    plot!(plt1, X1[:], Y1[i,:], marker=:o, label="")#, yerror=Yerr[i,:]κ="*string(round(K, digits=3))
 end
 display(plt1)
 #savefig(plt1, "fss_raw_data_d$(dim).png")
 
-# Plot after collapse
-plt2 = plot(title="Data collapse (after optimal shifts) d=$(dim)",
+#collapse plot
+plt2 = plot(title="Data collapse (after optimal shifts) d=$(dim) "*L"E_k, a_s="*a_s,
     xlabel="ln(ξ/N^(1/d))", ylabel="ln(Λ)")
 for (i,K) in enumerate(K_vals)
-    plot!(plt2, X[:] .+ shifts[i], Y[i,:], marker=:o, label="")
+    plot!(plt2, X1[:] .+ shifts1[i], Y1[i,:], marker=:o, label="")
 end
 display(plt2)
 #savefig(plt2, "fss_collapsed_data_d$(dim).png")
 
-println("Fit quality: ", s_rel)
+
+# plots for nc^2
+plt3 = plot(title="Raw data "*L"1/n_c^2, a_s="*a_s,
+    xlabel="ln(t^(-1/d))", ylabel="ln(Λ)")
+for (i,K) in enumerate(K_vals)
+    plot!(plt3, X2[:], Y2[i,:], marker=:o, label="")#, yerror=Yerr[i,:]κ="*string(round(K, digits=3))
+end
+display(plt3)
+#savefig(plt3, "fss_raw_data_nc2_d$(dim).png")
+
+#collapse plot
+plt4 = plot(title="Data collapse (after optimal shifts) d=$(dim) "*L"1/n_c^2, a_s="*a_s,
+    xlabel="ln(ξ/N^(1/d))", ylabel="ln(Λ)")
+for (i,K) in enumerate(K_vals)
+    plot!(plt4, X2[:] .+ shifts2[i], Y2[i,:], marker=:o, label="")
+end
+display(plt4)
+#savefig(plt4, "fss_collapsed_data_nc2_d$(dim).png")
+
+println("Fit quality 1: ", s_rel1)
+println("Fit quality 2: ", s_rel2)
