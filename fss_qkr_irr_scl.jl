fa7da0286ea1d087d522a175dea78c9615ebd92c
Your Name <eeaguilera99@gmail.com>
2025-11-27 14:53:02 +0100

    tidy code

--- fss_qkr_irr_scl.jl
+++ fss_qkr_irr_scl.jl
@@ -4,7 +4,7 @@ include("fss_qkr3_timescaling.jl")
 dim = 3
 
 # Replace with the path to your actual CSV file
-filepath = "irr_scale_data\\param_mR=2.csv"
+filepath = "irr_scale_data\\param_mR=1.csv"
 
 # Define aliases
 rename_map = Dict("nu" => "α", "c0" => "ψ")
@@ -65,7 +65,7 @@ t_vals = t_vals[5:end]
 p2_mat = p2_mat[:,5:end]
 
 function χ(K)
-    return (b0*-(Kc-K) + b1*(-(Kc-K))^2)# + b1*((Kc-K)/Kc)^2
+    return (b0*-(Kc-K) + 0*(-(Kc-K))^2 + 0*(-(Kc-K))^2)# + b1*((Kc-K)/Kc)^2
 end
 
 function scaling_funct(K, t)
@@ -80,61 +80,51 @@ function inverse_ξ(K)
     return abs(χ(K))^(ν)
 end
 
-F = [scaling_funct(k, t) for k in K_vals, t in t_vals]
-  #matrix of scaled data
 
-X = [χ(k)*t^(1/α) for k in K_vals, t in t_vals]  #matrix of scaled x-coordinates
-
-
+# One-parameter scaling collapse
 res, shifts, X1, Y1, Y1err, s_rel = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=1, n_kicks_f=0)
 
-
-#=
-plt1 = plot(title="fss_qkr_irr_scaling d=$(dim)",
-    xlabel="ln(ξ/N^(1/d))", ylabel="ln(Λ)",
-    size=(800,600))
-scatter!(-(α/3).*log.(abs.(X)), F, mc=:blue)
-display(plt1)=#
-
-Λ = p2_mat ./ (t_vals' .^ (2/dim))
-ln_Λ_c = corrected_scaling_funct(K_vals, t_vals, log.(Λ))
+# Compute scaled data
+F = [scaling_funct(k, t) for k in K_vals, t in t_vals]
+X = [χ(k)*t^(1/α) for k in K_vals, t in t_vals]  #matrix of scaled x-coordinates
+ln_Λ_c = corrected_scaling_funct(K_vals, t_vals, Y1) #matrix of corrected lnΛ values
 
 
 #plot irr scaling fit results
-plt2 = plot(title=latexstring("Finite-time scaling with correction \$d=$(dim)\$"),
+plt1 = plot(title=latexstring("Finite-time scaling with correction \$d=$(dim)\$"),
     xlabel=latexstring("ln \$(ξ/t^{1/d})\$"), ylabel=latexstring("ln \$(Λ)\$"))
 scatter!(vec(-(ν).*log.(abs.(X))), vec(ln_Λ_c), mc=:red, label="Corrected")
 #scatter!(-(α/3).*log.(abs.(X)), F, mc=:blue, label="")
-scatter!(vec(X1 .+ shifts) .+ 2.2, vec(Y1), mc=:blue, label="Uncorrected")
+scatter!(vec(X1 .+ shifts) , vec(Y1), mc=:blue, label="Uncorrected")
 #xlims!(-1, 7)
-display(plt2)
+display(plt1)
 
-
+#=
 #plot correlation length from fit
-plt3 = plot(title=latexstring("Correlation length from fit \$d=$(dim)\$"),
+plt2 = plot(title=latexstring("Correlation length from fit \$d=$(dim)\$"),
     xlabel=latexstring("\$κ\$"), ylabel=latexstring("\$ξ(κ)\$"))
 ξ_fit = [1/inverse_ξ(k) for k in K_vals]
 scatter!(K_vals, ξ_fit, mc=:green, label="ξ from fit")
 K_grid = range(minimum(K_vals), maximum(K_vals), length=400)
-plot!(plt3, K_grid, [1/inverse_ξ(k) for k in K_grid], lw=2, label="")
-vline!(plt3, [Kc], linestyle=:dash, color=:red, label=L"\kappa_c="*"$(round(Kc,digits=3))")
-display(plt3)
+plot!(plt2, K_grid, [1/inverse_ξ(k) for k in K_grid], lw=2, label="")
+vline!(plt2, [Kc], linestyle=:dash, color=:red, label=L"\kappa_c="*"$(round(Kc,digits=3))")
+display(plt2)
 
-#=
+
 # One-parameter scaling collapse using ξ from fit
 fit_shifts = log.( [1/inverse_ξ(k) for k in K_vals] )
-plt4 = plot(title=latexstring("Collapse using \$ξ\$ from fit \$d=$(dim)\$"),
+plt3 = plot(title=latexstring("Collapse using \$ξ\$ from fit \$d=$(dim)\$"),
     xlabel=latexstring("ln \$(ξ/N^{1/d})\$"), ylabel=latexstring("ln \$(Λ)\$"))
 for (i,K) in enumerate(K_vals)
-    plot!(plt4, X1[:] .+ fit_shifts[i], Y1[i,:], yerror=Y1err[i,:], marker=:o, label="")
+    plot!(plt3, X1[:] .+ fit_shifts[i], Y1[i,:], yerror=Y1err[i,:], marker=:o, label="")
 end
-display(plt4)
+display(plt3)
 
 
 # Comparison plot
-plt5 = plot(title="One-parameter scaling collapse vs Corrections",
+plt4 = plot(title="One-parameter scaling collapse vs Corrections",
     xlabel=latexstring("ln \$(ξ/N^{1/d})\$"), ylabel=latexstring("ln \$(Λ)\$"))
-scatter!(plt5, vec(X1 .+ [fit_shifts[i] for i in 1:length(K_vals), t in 1:size(X1,2)]), vec(Y1), mc=:red, marker=:o, 
+scatter!(plt4, vec(X1 .+ [fit_shifts[i] for i in 1:length(K_vals), t in 1:size(X1,2)]), vec(Y1), mc=:red, marker=:o, 
 label=latexstring("\$ξ\$ from corrections"))
-scatter!(plt5, vec(X1 .+ [shifts[i] for i in 1:length(K_vals), t in 1:size(X1,2)]).+ 2.05, vec(Y1), mc=:blue, marker=:o, label="1-param scaling")
-display(plt5)=#
+scatter!(plt4, vec(X1 .+ [shifts[i] for i in 1:length(K_vals), t in 1:size(X1,2)]).+ 2.05, vec(Y1), mc=:blue, marker=:o, label="1-param scaling")
+display(plt4)=#
