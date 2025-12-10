e8a8b6f5d05b72932080a59eb862c06685191f8d
Your Name <eeaguilera99@gmail.com>
2025-12-10 16:32:02 +0100

    upd2

--- fss_qrk3_Kc.jl
+++ fss_qrk3_Kc.jl
@@ -49,7 +49,7 @@ end
 dim = 3 # spatial dimension
 
 # Perform collapse
-res, shifts, X, Y, Yerr, s_rel, shifts_err = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=5, n_kicks_f=0)
+res, shifts, X, Y, Yerr, s_rel = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, n_kicks_i=5, n_kicks_f=0)
 # ===== Example usage =====
 xi = exp.(shifts)
 results = fit_xi_offset_LsqFit(K_vals, xi)
