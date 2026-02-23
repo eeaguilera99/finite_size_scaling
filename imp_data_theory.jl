57de1f0b6b1881ab38b51a448ee6ffe4841d5229
Your Name <eeaguilera99@gmail.com>
2026-02-23 09:08:55 +0100

    work x th

--- imp_data_theory.jl
+++ imp_data_theory.jl
@@ -6,8 +6,7 @@ K_vals = vec(Matrix(CSV.read("dataMF/kappa.csv", DataFrame; header=false)))     
 t_vals_0 = vec(Matrix(CSV.read("dataMF/d=5_horizontal_axis.csv", DataFrame; header=false)))  # Times
 p2_mat_0 = Matrix(CSV.read("dataMF/d=3_scaled_kinetic_energy_$(a_s).csv", DataFrame; header=false))
 nc_mat = Matrix(CSV.read("dataMF/d=3_scaled_nc_2_$(a_s).csv", DataFrame; header=false))              # ⟨p²⟩ values
-p2_err_mat = 0.01 .* p2_mat  # assume 1% error if no data
-nc_err_mat = 0.01 .* nc_mat 
+
 "Theory values of time are scaled, we revert them for dimension d1
 For p2 values, the matrix is scaled but also rows are t values and 
 columns are k values, we revert and transpose for dimension d2"
@@ -20,3 +19,5 @@ end
 dim1 = 5
 dim2 = 3
 t_vals, p2_mat, nc_mat = revert_scale(t_vals_0, p2_mat_0, nc_mat_0, dim1, dim2)
+p2_err_mat = 0.01 .* p2_mat  # assume 1% error if no data
+nc_err_mat = 0.01 .* nc_mat 
