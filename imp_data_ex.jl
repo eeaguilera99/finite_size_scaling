4995f1c5d04c14be119d59bf8b35b50a07e22edd
Your Name <eeaguilera99@gmail.com>
2026-03-20 14:06:40 +0100

    add intercation data

--- imp_data_ex.jl
+++ imp_data_ex.jl
@@ -1,9 +1,10 @@
 include("fss_qkr3_timescaling.jl")
 using CSV, DataFrames, Plots, LaTeXStrings
 
-a_s = 220
-K_vals = vec(Matrix(CSV.read("dataEX/$(a_s)/kappa.csv", DataFrame; header=false)))             # Kick strengths
-t_vals = vec(Matrix(CSV.read("dataEX/$(a_s)/number_of_kicks.csv", DataFrame; header=false)))  # Times
+a_s = "k=0.87"
+#K_vals = vec(Matrix(CSV.read("dataEX/$(a_s)/kappa.csv", DataFrame; header=false)))             # Kick strengths
+K_vals = vec(Matrix(CSV.read("dataEX/$(a_s)/a_s.csv", DataFrame; header=false)))             # interactions
+t_vals = vec(Matrix(CSV.read("dataEX/$(a_s)/Number_of_kicks.csv", DataFrame; header=false)))  # Times
 p2_mat = Matrix(CSV.read("dataEX/$(a_s)/nc_matrix.csv", DataFrame; header=false))   
 p2_err_mat = Matrix(CSV.read("dataEX/$(a_s)/nc_err_matrix.csv", DataFrame; header=false))     # Errors
 
@@ -25,13 +26,13 @@ Perform finite-time scaling collapse of Anderson transition data.
 """
 
 dim = 3 # spatial dimension
-transient = 4
-data_type = "Ex"
-shifts_data, X_data, Y_data, Yerr_data, s_rel_data = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; nbins=100, d=dim, n_kicks_i=transient, n_kicks_f=0)
+transient = 5
+data_type = "Ex int"
+shifts_data, X_data, Y_data, Yerr_data, s_rel_data = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; nbins=50, d=dim, n_kicks_i=transient, n_kicks_f=0)
 _, shiftserr_data, _ = shifts_parametric_mc(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, nbins=30, nmc=1000)
 
 
-#include("fss_qrk3_timescaling_analysis.jl")
+include("fss_qrk3_timescaling_analysis.jl")
 
-perform_collapse(K_vals, X_data, Y_data, Yerr_data, shifts_data; data_type="Ex nc^-2", raw=false, d=dim)
-perform_collapse_quality(K_vals, X_data, Y_data, Yerr_data, shifts_data, s_rel_data, dim, a_s, "Ex"; Kc_offset=2, plotshow=false)
+perform_collapse(K_vals, X_data, Y_data, Yerr_data, shifts_data; data_type=data_type, raw=true, d=dim)
+#perform_collapse_quality(K_vals, X_data, Y_data, Yerr_data, shifts_data, s_rel_data, dim, a_s, data_type; Kc_offset=2, plotshow=false)
