786587013882961e49433e81a95edbc2831a6ba4
Your Name <eeaguilera99@gmail.com>
2026-04-10 13:44:10 +0200

    working

--- fss_qkr3_tevol.jl
+++ fss_qkr3_tevol.jl
@@ -11,11 +11,14 @@ Note: Uses Kc from fss_qrk3_Kc.jl
 #perform_tevol(dim, shifts_data, t_vals, p2_mat, p2_err_mat; Kc_offset_index=Kc_offset_index, data_type="Ex \$1/n_c^2\$")
 
 #perform_tevol_transient(d1, shifts_data, t_vals, p2_mat, p2_err_mat; Kc_offset_index=Kc_offset_index, data_type="Ex \$1/n_c^2\$",
-#                            n_kicks_i=transient, n_kicks_f=0)
+#                          n_kicks_i=transient, n_kicks_f=0)
+Kc_offset_index = 0
+perform_tevol_powerlaw(D, shifts_data, t_vals, p2_mat, p2_err_mat; transient=2, Kc_offset_index=Kc_offset_index, data_type=data_type)
+#=
 K_1 = argmax(shifts_data) + Kc_offset_index
 K_2 = 16
 
 plt1 = scatter(t_vals, p2_mat[K_2, :], yerror=p2_err_mat[argmax(shifts_data), :], 
 label=latexstring("Data \$κ_c=$(round(K_vals[argmax(shifts_data)+Kc_offset_index], digits=3))\$"), xlabel="Time (N kicks)", 
 ylabel="⟨p²⟩", title=latexstring("Time evolution at criticality \$d=$dim\$, $data_type, \$a_s=$a_s\$"), legend=:topleft, xscale=:log10, yscale=:log10)
-display(plt1)
+display(plt1)=#
