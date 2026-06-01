fd39f1c6d7ec458aa928de01c077ababe8f8ecb0
Your Name <eeaguilera99@gmail.com>
2026-06-01 15:30:58 +0200

    working kc_offset=0

--- fss_qkr_scandim.jl
+++ fss_qkr_scandim.jl
@@ -1,12 +1,13 @@
 include("imp_data_ex.jl")
 include("fss_qkr3_timescaling.jl")  # for finite_time_scaling
+include("fss_qrk3_timescaling_analysis.jl")
 
 #code to loop over dimension values for best collapse
 
 d_vals = 1:0.5:10
 
 
-
+#=
 function dim_scan(d_values)
 
     collapse_quality = Float64[]
@@ -32,7 +33,24 @@ function dim_scan(d_values)
     display(plt_quality)
 end
 
-dim_scan(d_vals)
+dim_scan(d_vals)=#
+
+function Kc_scan(d_values)
+    Kc_values = Float64[]
+    for dim in d_values
+        shifts, _, _, _, _ = finite_time_scaling(K_vals, t_vals, p2_mat, p2_err_mat; nbins=50, d=dim, n_kicks_i=2)
+        _, shiftserr, _ = shifts_parametric_mc(K_vals, t_vals, p2_mat, p2_err_mat; d=dim, nbins=30, nmc=1000)
+        Kc = perform_Kc_anal(shifts, shiftserr, K_vals; data_type="Ex", d=dim, n_k_filter=0, K_guess_index=0, show_xierr=false, save=false)[4]
+        push!(Kc_values, Kc)
+    end
+
+    # Plot Kc vs dimension
+    plt_Kc = plot(d_values, Kc_values, lw=1, marker=:o,
+        xlabel="Dimension d", ylabel=L"K_c",
+        title="Critical kick strength Kc vs dimension d", label="")
+    display(plt_Kc)
+end
+Kc_scan(d_vals)
 
 #=
 # Optional: Plot best collapse curves
